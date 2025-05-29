a8d069ac6d0921ad335cf7fd1ed324a6493361b2
Junior <junior.jr.jl@gmail.com>
2025-05-29 19:00:34 -0300

    feat: create spring project

---
+++ src/main/kotlin/com/algaworks/algasensors/temperature/processing/AlgasensorsTemperatureProcessingApplication.kt
@@ -0,0 +1,11 @@
+package com.algaworks.algasensors.temperature.processing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlgasensorsTemperatureProcessingApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlgasensorsTemperatureProcessingApplication>(*args)
+}
